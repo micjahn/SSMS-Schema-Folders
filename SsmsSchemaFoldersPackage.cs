ed797022860d213a3d77b286bb535d178317c1c6
Nicholas <nicholas.ross@live.com.au>
2024-11-22 15:01:45 +1100

    Change Package to AsyncPackage

--- SsmsSchemaFoldersPackage.cs
+++ SsmsSchemaFoldersPackage.cs
@@ -30,14 +30,14 @@
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(SsmsSchemaFoldersPackage.PackageGuidString)]
-    [ProvideAutoLoad("d114938f-591c-46cf-a785-500a82d97410")] //CommandGuids.ObjectExplorerToolWindowIDString
+    [ProvideAutoLoad("d114938f-591c-46cf-a785-500a82d97410", PackageAutoLoadFlags.BackgroundLoad)] //CommandGuids.ObjectExplorerToolWindowIDString
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(SchemaFolderOptions), "SQL Server Object Explorer", "Schema Folders", 114, 116, true)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class SsmsSchemaFoldersPackage : Package, IDebugOutput
+    public sealed class SsmsSchemaFoldersPackage : AsyncPackage, IDebugOutput
     {
         /// <summary>
         /// SsmsSchemaFoldersPackage GUID string.
@@ -68,9 +68,14 @@
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
-        protected override void Initialize()
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
             // OutputWindowPane for debug messages
 #if DEBUG
@@ -267,7 +272,7 @@
 
                     e.Node.TreeView.Cursor = Cursors.AppStarting;
 
-                    var nodeExpanding = new Timer();
+                    var nodeExpanding = new System.Windows.Forms.Timer();
                     nodeExpanding.Interval = 10;
                     EventHandler nodeExpandingEvent = null;
                     nodeExpandingEvent = (object o, EventArgs e2) =>
